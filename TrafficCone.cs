fdd869237f3f906495f7c63806029fcbd92e317f
Karol Byczkowski <karol.byczkowski@lge.com>
2020-02-05 10:45:57 -0800

    Updated TrafficCone class to the network changes.
    TrafficCone prefab gained DistributedObject and DistributedRigidbody components.

--- TrafficCone.cs
+++ TrafficCone.cs
@@ -5,44 +5,30 @@
  *
  */
 
-using System.Net;
 using System.Collections.Generic;
 using UnityEngine;
-using Simulator.Network.Core.Shared.Connection;
-using Simulator.Network.Core.Shared.Messaging;
-using Simulator.Network.Core.Shared.Messaging.Data;
 
 namespace Simulator.Controllable
 {
-    public class TrafficCone : MonoBehaviour, IControllable, IMessageSender, IMessageReceiver
+    public class TrafficCone : MonoBehaviour, IControllable
     {
         public bool Spawned { get; set; }
         public string UID { get; set; }
-        public string Key => UID;
-        public string ControlType { get; set; } = "cone";
+        public string ControlType { get; set; }
         public string CurrentState { get; set; }
-        public string[] ValidStates { get; set; } = new string[] { };
-        public string[] ValidActions { get; set; } = new string[] { };
-        public string DefaultControlPolicy { get; set; } = "";
+        public string[] ValidStates { get; }
+        public string[] ValidActions { get; }
+        public string DefaultControlPolicy { get; set; }
         public string CurrentControlPolicy { get; set; }
-
-        private MessagesManager messagesManager;
-
+        
         private void Awake()
         {
             CurrentControlPolicy = DefaultControlPolicy;
             CurrentState = "";
         }
 
-        private void Start()
+        protected void OnDestroy()
         {
-            messagesManager = SimulatorManager.Instance?.Network.MessagesManager;
-            messagesManager?.RegisterObject(this);
-        }
-
-        private void OnDestroy()
-        {
-            messagesManager?.UnregisterObject(this);
             Resources.UnloadUnusedAssets();
         }
 
@@ -60,51 +46,6 @@ namespace Simulator.Controllable
                         break;
                 }
             }
-
-            if (SimulatorManager.Instance.Network.IsMaster && controlActions.Count > 0)
-            {
-                //Forward control actions to clients
-                var serializedControlActions = new BytesStack();
-                for (var i = 0; i < controlActions.Count; i++)
-                {
-                    var controlAction = controlActions[i];
-                    serializedControlActions.PushString(controlAction.Action);
-                    serializedControlActions.PushString(controlAction.Value);
-                }
-                BroadcastMessage(new Message(Key, serializedControlActions, MessageType.ReliableOrdered));
-            }
-        }
-
-        public void ReceiveMessage(IPeerManager sender, Message message)
-        {
-            var controlActions = new List<ControlAction>();
-            while (message.Content.Count > 0)
-            {
-                var action = message.Content.PopString();
-                var value = message.Content.PopString();
-                controlActions.Add(new ControlAction()
-                {
-                    Action = action,
-                    Value = value
-                });
-            }
-            if (controlActions.Count > 0)
-                Control(controlActions);
-        }
-
-        public void UnicastMessage(IPEndPoint endPoint, Message message)
-        {
-            messagesManager?.UnicastMessage(endPoint, message);
-        }
-
-        public void BroadcastMessage(Message message)
-        {
-            messagesManager?.BroadcastMessage(message);
-        }
-
-        void IMessageSender.UnicastInitialMessages(IPEndPoint endPoint)
-        {
-            //TODO support reconnection - send instantiation messages to the peer
         }
     }
 }
